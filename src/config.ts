b1605a82366b42521043c02b0069118131e3d7c9
Utsob Roy <roy@utsob.me>
2025-06-26 16:11:01 +0600

    fix: og image

--- src/config.ts
+++ src/config.ts
@@ -5,7 +5,7 @@ export const SITE: Site = {
   author: "Debdash Mistry",
   desc: "বাঙলাপাঠ সামান্যপাঠ-প্রভাবিত একটি কর্মশালা। এর উদ্দেশ্য বাঙলার ভাষা, শিল্প, ভূপ্রকৃতি, জনপদ, নৃসংস্থান, লোকাচার, পুরাকীর্তি ও ইতিহাসের একটি নির্ভরযোগ্য পাঠ নির্মাণে সহায়ক ভূমিকা রাখা। কোনোপ্রকার জাতীয়তাবাদের উগ্রতা, আগ্রাসন বা উন্নাসিকতা উসকে দেওয়া এই কর্মশালার লক্ষ্য নয়।",
   title: "বাঙলাপাঠ",
-  ogImage: "default-og.png",
+  ogImage: "/assets/default-og.png",
   lightAndDarkMode: false,
   postPerPage: 3,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
